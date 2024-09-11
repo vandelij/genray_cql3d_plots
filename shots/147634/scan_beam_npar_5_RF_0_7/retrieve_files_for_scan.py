3d8f39b9237480539af557da41c6080a49995abd
Van De Lindt <vandelij@MFEWS02.PSFC.MIT.EDU>
2024-09-11 19:00:44 -0400

    added beam scan at 700 kW RF

---
+++ shots/147634/scan_beam_npar_5_RF_0_7/retrieve_files_for_scan.py
@@ -0,0 +1,22 @@
+import os, sys
+#these shenanigans relate to vscode not having the working directory as the directory of the file it runs
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+host = 'eofe7.mit.edu'
+username = 'vandelij'
+rwdir = open("../../../../remoteDirectory.txt", "r").readlines()[0].strip()
+shotNum = rwdir.split('/')[-1].split('_')[1]
+
+print(f'Looking in shotNum {shotNum}')
+
+# Detailed scan 
+
+npars = [5] # [1,2,3,4,5,6,7,8,9,10,11]
+
+for npar in npars:
+    print(f'npar = {npar}..')
+    os.system(f'scp {username}@{host}:{rwdir}/scan_matrix_npar_beam_5_rf_0_7/npar_{npar}/cql3d.nc cql3d_npar_{npar}.nc')
+    os.system(f'scp {username}@{host}:{rwdir}/scan_matrix_npar_beam_5_rf_0_7/npar_{npar}/cql3d_krf001.nc cql3d_krf_npar_{npar}.nc')
+    os.system(f'scp {username}@{host}:{rwdir}/scan_matrix_npar_beam_5_rf_0_7/npar_{npar}/cql3d.ps cql3d_rfpwr_npar_{npar}.ps')
